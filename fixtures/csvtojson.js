65be199c5f262c536062ee64ccb1427be4269ef0
juiceqa <gglick@juicepharma.com>
2023-03-21 10:22:21 -0600

    adding .gitignore

--- fixtures/csvtojson.js
+++
@@ -1,4 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
-let fileInputName = '04-42-nodups.csv';
-let fileOutputName = 'set16.json';
-csvToJson.formatValueByType().fieldDelimiter(',').generateJsonFileFromCsv(fileInputName, fileOutputName)
